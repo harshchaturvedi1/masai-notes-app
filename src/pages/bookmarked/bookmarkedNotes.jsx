68fd4c4210788ddfbaab91d9d79889b30962a5a2
harsh chaturvedi <harshchaturvedi589@gmail.com>
2023-01-24 11:17:47 +0530

    styling

--- src/pages/bookmarked/bookmarkedNotes.jsx
+++ src/pages/bookmarked/bookmarkedNotes.jsx
@@ -4,12 +4,69 @@ export const BookMarkedNotes=()=>{
 
     const [allnotes, setAllNotes] = useState([])
 
+    const handleDelete = (id) => {
+        const newItems = []
+        const tempNotes =  JSON.parse(localStorage.getItem('masai-notes')) || []
+        tempNotes.map(ele => {
+            if (ele.id !== id)
+                newItems.push(ele)
+        })
+
+        localStorage.setItem('masai-notes', JSON.stringify(newItems))
+        setAllNotes([...newItems])
+
+    }
+
+    const handleBookmark = (id) => {
+        const newItems = []
+
+        const tempNotes =  JSON.parse(localStorage.getItem('masai-notes')) || []
+
+        tempNotes.map(ele => {
+            if (ele.id === id) {
+                console.log(ele)
+                ele.bookmark = false
+                newItems.push(ele)
+            }
+            else
+                newItems.push(ele)
+        })
+
+        localStorage.setItem('masai-notes', JSON.stringify(newItems))
+        setAllNotes([...newItems])
+    }
+
     useEffect(() => {
         const allItems = JSON.parse(localStorage.getItem('masai-notes')) || []
-        setAllNotes([...allItems])
+        const allBookmarks = []
+        allItems.map(ele=>{
+            ele.bookmark && allBookmarks.push(ele)
+        })
+        setAllNotes([...allBookmarks])
     }, [])
 
     return <>
     <h3>All you bookmarked</h3>
+    {
+            allnotes.length > 0 ? (<div>
+
+                {
+                    allnotes.map(ele => (
+                      ele.bookmark &&  <div key={ele?.id}>
+                            <div>
+
+                                <div>{ele?.title}</div>
+                                <div>{ele?.description}</div>
+                                <div>{ele?.date}</div>
+                            </div>
+                            <div>
+                                <button onClick={() => handleDelete(ele.id)}>Delete</button>
+                                <button onClick={() => handleBookmark(ele.id)}>Remove Bookmark</button>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>) : <div>No item found!</div>
+        }
     </>
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import styles from "./home.module.css"
 export const Home = () => {
     return <div>
-        <h3>Masai Notes App</h3>
-        <div>
-            <Link to="/newnote">new note</Link>
+        <div className={styles.heading}>Masai Notes App</div>
+        <div className={styles.allRouters}>
+            <Link to="/newnote">New note</Link>
             <Link to="/bookmarks">Bookmarks</Link>
             <Link to="/allnotes">View All Notes</Link>
         </div>
